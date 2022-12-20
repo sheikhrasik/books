96bc8f4396651b2cfd4c77e07211e396e8d1c8b6
sheikhrasik <sheikhrasik101@gmail.com>
2022-12-20 06:23:51 +0530

    final refactor using custom hooks.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useEffect , useContext } from "react";
+import { useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
-import BooksContext from "./context/books";
+import useBooksContext from "./hooks/use-books-context";
 
 function App() {
-  const { fetchBooks } = useContext(BooksContext);
+  const { fetchBooks } = useBooksContext();
 
   useEffect(() => {
     fetchBooks();
-  },[])
+  })
 
   return (
     <div className="app">
